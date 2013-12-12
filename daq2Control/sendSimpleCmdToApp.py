6085d102c34fa88049fbdd20e93df8fe17e9dc19
Benjamin Stieger <stiegerb@cern.ch>
2013-12-12 13:54:19 +0100

    Added sendSimpleCmdToApp.py

---
+++ daq2Control/sendSimpleCmdToApp.py
@@ -0,0 +1,35 @@
+#! /usr/bin/env python
+from daq2Control import separator
+from daq2SymbolMap import daq2SymbolMap
+from daq2Utils import sendSimpleCmdToApp, printError
+
+if __name__ == "__main__":
+	from optparse import OptionParser
+	usage="""
+	%prog [options] hostname application instance command
+
+	where hostname is defined in symbolmap
+	"""
+	parser = OptionParser(usage=usage)
+	parser.add_option("-v", "--verbose", default=0,     action="store", type='int', dest="verbose", help="Set the verbose level, [default: %default (quiet)]")
+	parser.add_option("--dry",           default=False, action="store_true",        dest="dry",     help="Just print the command without sending anything")
+	(options, args) = parser.parse_args()
+
+	try:
+		d2SM  = daq2SymbolMap()
+		host = d2SM(args[0])
+		classname, instance, command = args[1], int(args[2]), args[3]
+
+		sendSimpleCmdToApp(host.host, host.port, classname, instance, command, verbose=options.verbose, dry=options.dry)
+		exit(0)
+	except KeyError:
+		printError('Host %s not defined in symbolmap!'%args[0])
+		exit(-1)
+	except IndexError:
+		printError('Not enough arguments!')
+		parser.print_help()
+		exit(-1)
+
+	parser.print_help()
+	exit(-1)
+
