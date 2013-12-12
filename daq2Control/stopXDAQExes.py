592e721f154d49db59c3afbac927d4c438604b7f
Benjamin Stieger <stiegerb@cern.ch>
2013-12-12 16:09:46 +0100

    Added stopXDAQExes.py

---
+++ daq2Control/stopXDAQExes.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+from daq2Control import separator
+from daq2SymbolMap import daq2SymbolMap
+from daq2Utils import stopXDAQs
+
+if __name__ == "__main__":
+	from optparse import OptionParser
+	usage="""
+	%prog [options]
+	"""
+	parser = OptionParser(usage=usage)
+	parser.add_option("-v", "--verbose", default=0,     action="store", type='int', dest="verbose", help="Set the verbose level, [default: %default (quiet)]")
+	parser.add_option("--dry",           default=False, action="store_true",        dest="dry",     help="Just print the command without sending anything")
+	(options, args) = parser.parse_args()
+
+	d2SM  = daq2SymbolMap()
+	stopXDAQs(d2SM, verbose=options.verbose, dry=options.dry)
+	print separator
+	exit(0)
