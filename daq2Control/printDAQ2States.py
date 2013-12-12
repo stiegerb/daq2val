41933a588b21085c4ddfac43cfb8fcb0c2bf3f17
Benjamin Stieger <stiegerb@cern.ch>
2013-12-12 13:31:08 +0100

    Cleanup

--- daq2Control/printDAQ2States.py
+++ daq2Control/printDAQ2States.py
@@ -3,30 +3,7 @@ import daq2Utils as utils
 from daq2Config import daq2Config
 from daq2Control import separator
 from daq2SymbolMap import daq2SymbolMap
-from daq2Utils import sleep, printError, printWarningWithWait, loadMonitoringItemsFromURL, getParam
-
-def printApplicationState(host, classname, instance, statename, dry=False):
-	## Special case until stateName is added to the application infospace in the FerolController
-	if dry: return True
-	if host.type == 'FEROLCONTROLLER':
-		url = 'http://%s:%d/urn:xdaq-application:lid=109' % (host.host, host.port)
-		items = loadMonitoringItemsFromURL(url)
-		state = items['stateName']
-		if not state == statename:
-			stdout.write('\n')
-			printError('Application %s, instance %d, on host %s:%-d, did not return state "%s", instead was "%s".' %(classname, instance, host.host, host.port, statename, state))
-			return False
-		return True
-
-	## Normal case
-	state = getParam(host.host, host.port, classname, instance, 'stateName', 'xsd:string')
-	state = state.strip('\n') ## remove trailing newlines
-	if not state == statename:
-		stdout.write('\n')
-		printError('Application %s, instance %d, on host %s:%-d, did not return state "%s", instead was "%s".' %(classname, instance, host.host, host.port, statename, state))
-		return False
-	return True
-
+from daq2Utils import loadMonitoringItemsFromURL, getParam
 
 if __name__ == "__main__":
 	from optparse import OptionParser
@@ -40,6 +17,7 @@ if __name__ == "__main__":
 	d2Cfg = daq2Config(args[0], verbose=options.verbose)
 	d2Cfg.fillFromSymbolMap(d2SM)
 
+	print separator
 	for host in d2Cfg.hosts:
 		# print host.name
 		for app,inst in host.applications:
@@ -54,6 +32,7 @@ if __name__ == "__main__":
 				state = getParam(host.host, host.port, app, inst, 'stateName', 'xsd:string')
 				state = state.strip('\n') ## remove trailing newlines
 			print'%-17s %-25s(%2d) %-20s' % (host.name, app, inst, state)
+	print separator
 
 	exit(0)
 
