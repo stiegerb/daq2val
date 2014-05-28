58cb4c8b71433d39343b9013fc2be0b541dd9feb
Benjamin Stieger <stiegerb@cern.ch>
2014-05-28 12:52:21 +0200

    Take config fragments from current directory by default

--- daq2Control/makeMSIOConfig.py
+++ daq2Control/makeMSIOConfig.py
@@ -35,8 +35,10 @@ def main(options, args):
 	nClients, nServers = getMSIOConfig(args[0])
 
 	if len(options.fragmentDir) == 0:
-		options.fragmentDir = ('/nfshome0/stiegerb/Workspace/'
-		                       'daq2val/daq2Control/config_fragments/')
+		# By default take the config_fragments dir from the current release
+		workingdir = os.path.dirname(os.path.realpath(__file__))
+		options.fragmentDir = os.path.join(workingdir,'config_fragments')
+
 	configurator = daq2MSIOConfigurator(options.fragmentDir,
 		                                verbose=options.verbose)
 
