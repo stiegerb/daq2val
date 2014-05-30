4d481c11440ba2eff1b2929e4982643c44378a48
Benjamin Stieger <stiegerb@cern.ch>
2014-05-30 10:28:11 +0200

    First working version of Andys dynamic IBV configuration

--- daq2Control/daq2MSIOConfigurator.py
+++ daq2Control/daq2MSIOConfigurator.py
@@ -35,6 +35,7 @@ class daq2MSIOConfigurator(daq2Configurator):
 		## These should be passed as arguments
 		self.nclients = 1
 		self.nservers = 1
+		self.clientSendQPSize = None
 
 	def addMSIOI2OProtocol(self):
 		i2ons = "http://xdaq.web.cern.ch/xdaq/xsd/2004/I2OConfiguration-30"
@@ -68,102 +69,157 @@ class daq2MSIOConfigurator(daq2Configurator):
 				                   'instance':"%d"%n,
 				                   'tid':'%d'%(starting_tid+n)}))
 
+	def configureIBV(self, context):
+		maxMSize = int(self.readPropertyFromApp(context=context,
+			                        classname="pt::ibv::Application",
+			                        prop_name="maxMessageSize"))
+
+		if 'RU' in context.attrib['url']: # Client:
+			sendPoolSize = 64*1024*1024
+			recvPoolSize = 0x40000
+			complQPSize = max(sendPoolSize, recvPoolSize) / maxMSize
+			# print "max(%d, %d) / %d" % (sendPoolSize, recvPoolSize, maxMSize)
+			sendQPSize = sendPoolSize / maxMSize / self.nservers
+			self.clientSendQPSize = sendQPSize
+			recvQPSize = 2
+			if self.verbose > 1:
+				print "  client complQPSize", complQPSize
+				print "  client sendQPSize", sendQPSize
+			self.configureIBVApplication(context=context,
+				                         sendPoolSize=sendPoolSize,
+				                         recvPoolSize=recvPoolSize,
+				                         complQPSize=complQPSize,
+				                         sendQPSize=sendQPSize,
+				                         recvQPSize=recvQPSize)
+
+		elif 'BU' in context.attrib['url']: # Server:
+			sendPoolSize = 0x40000
+			if self.clientSendQPSize is not None:
+				recvQPSize = self.clientSendQPSize
+			else:
+				raise RuntimeError('Need to configure client IBV application before '
+					               'server IBV application!')
+			recvPoolSize = recvQPSize * maxMSize * self.nclients * 2
+			complQPSize = max(sendPoolSize, recvPoolSize) / maxMSize
+			sendQPSize = 2
+			if self.verbose > 1:
+				print "  server complQPSize", complQPSize
+				print "  server sendQPSize", sendQPSize
+
+			self.configureIBVApplication(context=context,
+				                         sendPoolSize=sendPoolSize,
+				                         recvPoolSize=recvPoolSize,
+				                         complQPSize=complQPSize,
+				                         sendQPSize=sendQPSize,
+				                         recvQPSize=recvQPSize)
+
+
+
 	def makeClient(self, index):
 		fragmentname = 'msio/client_context.xml'
-		ru_context = elementFromFile(os.path.join(self.fragmentdir,
+		context = elementFromFile(os.path.join(self.fragmentdir,
 			                                      fragmentname))
 
 		## Add policy
-		addFragmentFromFile(target=ru_context, filename=
+		addFragmentFromFile(target=context, filename=
 			                os.path.join(self.fragmentdir,
 			                	'msio/client_policy_%s.xml'% self.ptprot),
 			                index=0)
 		polns = "http://xdaq.web.cern.ch/xdaq/xsd/2013/XDAQPolicy-10"
-		for element in ru_context.findall(QN(polns,"policy").text+'/'+
+		for element in context.findall(QN(polns,"policy").text+'/'+
 			                              QN(polns,"element").text):
 			if 'RU%d' in element.get('pattern'):
 				element.set('pattern',element.get('pattern').replace(
 					                         'RU%d', 'RU%d'%(index)))
 		## Add pt application
-		addFragmentFromFile(target=ru_context, filename=
+		addFragmentFromFile(target=context, filename=
 			                os.path.join(self.fragmentdir,
 		                    'msio/client_%s_application.xml'% self.ptprot),
 		                    index=2) ## add after policy and endpoint
+
+		## Configure IBV application:
+		self.configureIBV(context)
+
 		## Add corresponding module
 		module = Element(QN(self.xdaqns, 'Module').text)
 		module.text = "$XDAQ_ROOT/lib/libpt%s.so"%self.ptprot
-		ru_context.insert(3,module)
+		context.insert(3,module)
 
 		## Client application
 		ru_app = elementFromFile(filename=os.path.join(self.fragmentdir,
 			                     'msio/client_application.xml'))
-		ru_context.insert(4,ru_app)
+		context.insert(4,ru_app)
 		ru_app.set('instance',str(index))
 
 		## Add corresponding module
 		module = Element(QN(self.xdaqns, 'Module').text)
 		if self.evbns == 'msio':
 			module.text = "$XDAQ_ROOT/lib/libmstreamio2g.so"
-		ru_context.insert(5,module)
+		context.insert(5,module)
 
 		## Set instance and url
-		for app in ru_context.findall(QN(self.xdaqns, 'Endpoint').text):
+		for app in context.findall(QN(self.xdaqns, 'Endpoint').text):
 			if 'RU%d' in app.attrib['hostname']:
 				app.set('hostname', app.get('hostname')%index)
 			if 'RU%d' in app.attrib['port']:
 				app.set('port', app.get('port')%index)
-		ru_context.set('url', ru_context.get('url')%(index, index))
+		context.set('url', context.get('url')%(index, index))
 
-		return ru_context
+		return context
+
 	def makeServer(self, index):
 		fragmentname = 'msio/server_context.xml'
-		bu_context = elementFromFile(os.path.join(self.fragmentdir,
+		context = elementFromFile(os.path.join(self.fragmentdir,
 			                                      fragmentname))
 
 		## Add policy
-		addFragmentFromFile(target=bu_context,
+		addFragmentFromFile(target=context,
 			                filename=os.path.join(self.fragmentdir,
 			                	     'msio/server_policy_%s.xml'%(
 			                	      self.ptprot)),
 			                index=0)
 		## Add pt application
-		addFragmentFromFile(target=bu_context,
+		addFragmentFromFile(target=context,
 		                    filename=os.path.join(self.fragmentdir,
 		                             'msio/server_%s_application.xml'%(
 		                             self.ptprot)),
 		                    index=2) ## add after the two endpoints
+
+		## Configure IBV application:
+		self.configureIBV(context)
+
 		## Add corresponding module
 		module = Element(QN(self.xdaqns, 'Module').text)
 		module.text = "$XDAQ_ROOT/lib/libpt%s.so"%self.ptprot
-		bu_context.insert(3,module)
+		context.insert(3,module)
 
 		## Server application
 		bu_app = elementFromFile(filename=os.path.join(self.fragmentdir,
 			                     'msio/server_application.xml'))
-		bu_context.insert(4,bu_app)
+		context.insert(4,bu_app)
 		bu_app.set('instance',str(index))
 
 		## Add corresponding module
 		module = Element(QN(self.xdaqns, 'Module').text)
 		if self.evbns == 'msio':
 			module.text = "$XDAQ_ROOT/lib/libmstreamio2g.so"
-		bu_context.insert(5,module)
+		context.insert(5,module)
 
 		## Set instance and url
-		for app in bu_context.findall(QN(self.xdaqns, 'Endpoint').text):
+		for app in context.findall(QN(self.xdaqns, 'Endpoint').text):
 			if 'BU%d' in app.attrib['hostname']:
 				app.set('hostname', app.get('hostname')%index)
 			if 'BU%d' in app.attrib['port']:
 				app.set('port', app.get('port')%index)
-		bu_context.set('url', bu_context.get('url')%(index, index))
+		context.set('url', context.get('url')%(index, index))
 
 		## Set instance and url
-		for app in bu_context.findall(QN(self.xdaqns, 'Application').text):
+		for app in context.findall(QN(self.xdaqns, 'Application').text):
 			if app.attrib['class'] != "%s::BU"%self.evbns: continue
 			app.set('instance', str(index))
 			break
 
-		return bu_context
+		return context
 
 	def makeRU(self, ruindex):
 		fragmentname = 'RU/gevb2g/msio/RU_context_msio.xml'
@@ -219,7 +275,7 @@ class daq2MSIOConfigurator(daq2Configurator):
 			if 'RU%d' in app.attrib['port']:
 				app.set('port', app.get('port')%ruindex)
 		ru_context.set('url', ru_context.get('url')%(ruindex, ruindex))
-		
+
 		self.setPropertyInApp(ru_context, 'gevb2g::InputEmulator',
 			                  'destinationClassInstance', str(ruindex))
 
