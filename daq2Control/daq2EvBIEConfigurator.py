a6b1144db8e7da1dc6b806235acea9fe546ef6f4
Benjamin Stieger <stiegerb@cern.ch>
2014-06-05 09:40:14 +0200

    First working version, untested config

---
+++ daq2Control/daq2EvBIEConfigurator.py
@@ -0,0 +1,274 @@
+import os
+import subprocess
+import re
+import time
+from copy import deepcopy
+
+from xml.etree import ElementTree
+from xml.etree.ElementTree import Element
+from xml.etree.ElementTree import SubElement
+from xml.etree.ElementTree import QName as QN
+from xml.parsers.expat import ExpatError
+
+from daq2Utils import printError, printWarningWithWait
+from daq2Configurator import elementFromFile, addFragmentFromFile
+from daq2Configurator import RU_STARTING_TID, BU_STARTING_TID
+
+
+######################################################################
+from daq2Configurator import daq2Configurator
+class daq2EvBIEConfigurator(daq2Configurator):
+	'''
+---------------------------------------------------------------------
+  class daq2EvBIEConfigurator
+
+---------------------------------------------------------------------
+'''
+	def __init__(self, fragmentdir, verbose=5):
+		super(daq2EvBIEConfigurator, self).__init__(fragmentdir,
+			                                       verbose=verbose)
+		self.evbns          = 'evb'
+		self.ptprot         = 'ibv' ## 'ibv' or 'udapl'
+
+	def configureIBVforEvBIE(self):
+		## TODO: Update!
+		RUFragmentPath = os.path.join(self.fragmentdir,
+                           'RU/gevb2g/msio/RU_ibv_application_msio.xml')
+		BUFragmentPath = os.path.join(self.fragmentdir,
+                           'BU/gevb2g/msio/BU_ibv_application_msio.xml')
+		EVMFragmentPath = os.path.join(self.fragmentdir,
+                           'EVM/msio/EVM_ibv_application_msio.xml')
+		EVMIBVApp = elementFromFile(filename=EVMFragmentPath)
+
+
+		RUIBVApp = elementFromFile(filename=RUFragmentPath)
+		BUIBVApp = elementFromFile(filename=BUFragmentPath)
+
+		BUApp = elementFromFile(filename=os.path.join(self.fragmentdir,
+				                  'BU/gevb2g/msio/BU_application_msio.xml'))
+		maxResources = int(self.readPropertyFromApp(
+		                        application=BUApp,
+		                        prop_name="maxResources"))
+
+		RUMaxMSize = int(self.readPropertyFromApp(
+			                        application=RUIBVApp,
+			                        prop_name="maxMessageSize"))
+
+		BUMaxMSize = int(self.readPropertyFromApp(
+			                        application=BUIBVApp,
+			                        prop_name="maxMessageSize"))
+		self.maxMessageSize = RUMaxMSize if RUMaxMSize == BUMaxMSize else None
+
+		# RU:
+		if self.RUSendQPSize is not None:
+			sendQPSize = self.RUSendQPSize
+		else:
+			sendQPSize = maxResources*self.nbus
+
+		if self.RUSendPoolSize is not None:
+			sendPoolSize = 1024*1024*self.RUSendPoolSize
+		else:
+			sendPoolSize = sendQPSize*RUMaxMSize
+
+		if self.RUComplQPSize is not None:
+			complQPSize = self.RUComplQPSize
+		else:
+			complQPSize = 8192
+
+		recvPoolSize = 0x2000000
+		recvQPSize = 64
+
+		self.RUIBVConfig = (sendPoolSize, recvPoolSize,
+			                complQPSize, sendQPSize, recvQPSize)
+
+		# BU:
+		if self.BURecvQPSize is not None:
+			recvQPSize = self.BURecvQPSize
+		else:
+			recvQPSize = int(self.RUIBVConfig[0]*2/self.nrus/BUMaxMSize)
+
+		if self.BURecvPoolSize is not None:
+			recvPoolSize = 1024*1024*self.BURecvPoolSize
+		else:
+			recvPoolSize = int((recvQPSize+maxResources)*self.nrus*BUMaxMSize)
+			# recvPoolSize = int(recvQPSize*self.nrus*BUMaxMSize*1.4)
+
+		if self.BUComplQPSize is not None:
+			complQPSize = self.BUComplQPSize
+		else:
+			complQPSize = recvQPSize*self.nrus
+
+		sendPoolSize = 0x2000000
+		sendQPSize = 64
+
+
+		self.BUIBVConfig = (sendPoolSize, recvPoolSize,
+			                complQPSize, sendQPSize, recvQPSize)
+
+		# EVM:?
+		sendPoolSize = maxResources*256*1024*self.nbus*2
+		recvPoolSize = maxResources*256*1024*self.nbus*2
+		recvQPSize = maxResources*2
+		sendQPSize = maxResources
+		complQPSize = maxResources*2*self.nbus
+
+
+		self.EVMIBVConfig = (sendPoolSize, recvPoolSize,
+			                complQPSize, sendQPSize, recvQPSize)
+
+	def makeRU(self, ruindex):
+		fragmentname = 'RU/evb/RU_context_bare.xml'
+		context = elementFromFile(os.path.join(self.fragmentdir,fragmentname))
+
+		## Add policy
+		addFragmentFromFile(target=context,
+			                filename=os.path.join(self.fragmentdir,
+			                'RU/evb/RU_policy_%s.xml'%
+			                self.ptprot),
+			                index=0)
+		polns = "http://xdaq.web.cern.ch/xdaq/xsd/2013/XDAQPolicy-10"
+		for element in context.findall(QN(polns,"policy").text+'/'+
+			                              QN(polns,"element").text):
+			if 'RU%d' in element.get('pattern'):
+				element.set('pattern',element.get('pattern').replace(
+					                         'RU%d', 'RU%d'%(ruindex)))
+		## Add builder network endpoint
+		context.insert(1,Element(QN(self.xdaqns, 'Endpoint').text,
+			              {'protocol':'%s'%self.ptprot ,
+			               'service':"i2o",
+			               'hostname':'RU%d_I2O_HOST_NAME'%(ruindex),
+			               'port':'RU%d_I2O_PORT'%(ruindex),
+			               'network':"infini"}))
+		## Add builder network pt application
+		addFragmentFromFile(target=context,
+		                    filename=os.path.join(self.fragmentdir,
+		                    'RU/evb/RU_%s_application.xml'%
+		                    self.ptprot),
+		                    index=2) ## add after the two endpoints
+
+		## Configure IBV application:
+		if self.setDynamicIBVConfig:
+			self.configureIBVApplication(context, self.RUIBVConfig)
+
+		## Add corresponding module
+		module = Element(QN(self.xdaqns, 'Module').text)
+		module.text = "$XDAQ_ROOT/lib/libpt%s.so"%self.ptprot
+		context.insert(3,module)
+
+		## RU application
+		ru_app_filename = os.path.join(self.fragmentdir,
+			                  'RU/evb/RU_application.xml')
+		if ruindex == 0:
+			ru_app_filename = os.path.join(self.fragmentdir,
+				              'RU/evb/RU_application_EVM.xml')
+
+		ru_app = elementFromFile(filename=ru_app_filename)
+
+		## Remove numberOfResponders, fragmentFIFOCapacity:
+		self.removePropertyInApp(ru_app, 'numberOfResponders')
+		self.removePropertyInApp(ru_app, 'fragmentFIFOCapacity')
+
+		## Set inputSource to Local:
+		self.setPropertyInApp(ru_app, 'inputSource', 'Local')
+
+		context.insert(5,ru_app)
+		ru_app.set('instance',str(ruindex))
+
+		ruevbappns = (self.xdaqappns%'evb::RU' if
+			          ruindex>0 else self.xdaqappns%'evb::EVM')
+		fedSourceIds = ru_app.find(QN(ruevbappns, 'properties').text+'/'+
+			                       QN(ruevbappns, 'fedSourceIds').text)
+		fedSourceIds.attrib[QN(self.soapencns, 'arrayType').text] = (
+			                       "xsd:ur-type[1]")
+
+		## Set instance and url
+		for app in context.findall(QN(self.xdaqns, 'Endpoint').text):
+			if 'RU%d' in app.attrib['hostname']:
+				app.set('hostname', app.get('hostname')%ruindex)
+			if 'RU%d' in app.attrib['port']:
+				app.set('port', app.get('port')%ruindex)
+		context.set('url', context.get('url')%(ruindex, ruindex))
+
+		return context
+	def makeBU(self, index):
+		fragmentname = 'BU/BU_context.xml'
+		context = elementFromFile(os.path.join(self.fragmentdir,
+			                                      fragmentname))
+
+		## Add policy
+		addFragmentFromFile(target=context,
+			                filename=os.path.join(self.fragmentdir,
+			                	'BU/evb/BU_policy_%s.xml'%(
+			                	self.ptprot)),
+			                index=0)
+		## Add builder network endpoint
+		context.insert(1,Element(QN(self.xdaqns, 'Endpoint').text, {
+			                        'protocol':'%s'%self.ptprot ,
+			                        'service':'i2o',
+			                        'hostname':'BU%d_I2O_HOST_NAME'%(index),
+			                        'port':'BU%d_I2O_PORT'%(index),
+			                        'network':'infini'}))
+		## Add builder network pt application
+		addFragmentFromFile(target=context,
+		                    filename=os.path.join(self.fragmentdir,
+		                        'BU/BU_%s_application.xml'%(
+		                        self.ptprot)),
+		                    index=2) ## add after the two endpoints
+
+		## Configure IBV application:
+		if self.setDynamicIBVConfig:
+			self.configureIBVApplication(context, self.BUIBVConfig)
+
+		## Add corresponding module
+		module = Element(QN(self.xdaqns, 'Module').text)
+		module.text = "$XDAQ_ROOT/lib/libpt%s.so"%self.ptprot
+		context.insert(3,module)
+
+		## BU application
+		bu_app = elementFromFile(filename=os.path.join(self.fragmentdir,
+			                     'BU/evb/BU_application.xml'))
+		self.removePropertyInApp(bu_app, 'maxEvtsUnderConstruction')
+		self.removePropertyInApp(bu_app, 'eventsPerRequest')
+		context.insert(4,bu_app)
+		bu_app.set('instance',str(index))
+
+		## Set instance and url
+		for app in context.findall(QN(self.xdaqns, 'Application').text):
+			if app.attrib['class'] != "%s::BU"%self.evbns: continue
+			app.set('instance', str(index))
+			break
+		context.set('url', context.get('url')%(index, index))
+
+		module = Element(QN(self.xdaqns, 'Module').text)
+		module.text = "$XDAQ_ROOT/lib/libevb.so"
+		context.insert(5,module)
+
+		return context
+
+	def makeEvBIEConfig(self, nrus=1, nbus=1,
+		               destination='configuration.template.xml'):
+		self.nrus = nrus
+		self.nbus = nbus
+
+		##
+		self.makeSkeleton()
+
+		if "ibv" in self.ptprot and self.setDynamicIBVConfig:
+			self.configureIBVforEvBIE()
+		else:
+			self.readIBVConfig()
+
+		if self.verbose > 1:
+			self.printIBVConfig()
+
+		self.addI2OProtocol()
+		for index in xrange(self.nrus):
+			self.config.append(self.makeRU(index))
+		for index in xrange(self.nbus):
+			self.config.append(self.makeBU(index))
+
+
+		self.writeConfig(destination)
+		if self.verbose>0: print 70*'-'
+
+
---
+++ daq2Control/makeEvBIEConfig.py
@@ -0,0 +1,125 @@
+#! /usr/bin/env python
+import os
+from daq2EvBIEConfigurator import daq2EvBIEConfigurator
+from daq2Utils import getConfig, printWarningWithWait, printError
+
+from makeDAQ2Config import getNxNConfig
+
+def addConfiguratorOption(parser):
+	parser.add_option("-d", "--setDynamicIBVConfig", default=False,
+		              action="store_true", dest="setDynamicIBVConfig",
+		              help="Set the senderPoolSize, receiverPoolSize, "
+		                   "completionQueueSize, sendQueuePairSize, "
+		                   "and recvQueuePairSize parameters of the "
+		                   "pt::ibv::Application dynamically according "
+		                   "to Andys algorithm. If not set, take "
+		                   "everything from fragment.")
+	parser.add_option("--cP", "--RUSendPoolSize", default=None,
+		              action="store", type="int", dest="RUSendPoolSize",
+		              help="Set the sendPoolSize parameter on the MStreamIO "
+		                   "RU (in MBytes, default: %default)")
+	parser.add_option("--cQ", "--RUSendQPSize", default=None,
+		              action="store", type="int", dest="RUSendQPSize",
+		              help="Set the sendQueuePairSize parameter on the "
+		                   "MStreamIO RU [default %default]")
+	parser.add_option("--cCQ", "--RUComplQPSize", default=None,
+		              action="store", type="int", dest="RUComplQPSize",
+		              help="Set the complQueuePairSize parameter on the "
+		                   "MStreamIO RU [default %default]")
+	parser.add_option("--sP", "--BURecvPoolSize", default=None,
+		              action="store", type="int", dest="BURecvPoolSize",
+		              help="Set the recvPoolSize parameter on the MStreamIO "
+		                   "BU (in MBytes, default: %default)")
+	parser.add_option("--sQ", "--BURecvQPSize", default=None,
+		              action="store", type="int", dest="BURecvQPSize",
+		              help="Set the recvQueuePairSize parameter on the "
+		                   "MStreamIO BU [default %default]")
+	parser.add_option("--sCQ", "--BUComplQPSize", default=None,
+		              action="store", type="int", dest="BUComplQPSize",
+		              help="Set the complQueuePairSize parameter on the "
+		                   "MStreamIO BU [default %default]")
+	parser.add_option("--fragmentDir", default='', action="store",
+		              type="string", dest="fragmentDir",
+		              help=("Use config fragments from a directory other "
+		                    "than the default"))
+	parser.add_option("-v", "--verbose", default=1, action="store",
+		              type='int', dest="verbose",
+		              help=("Set the verbose level, [default: %default "
+		              		"(semi-quiet)]"))
+	parser.add_option("-o", "--output", default='', action="store",
+		              type='string', dest="output",
+		              help="Where to put the output file")
+
+def main(options, args):
+	nRUs, nBUs = getNxNConfig(args[0])
+
+	if len(options.fragmentDir) == 0:
+		# By default take the config_fragments dir from the current release
+		workingdir = os.path.dirname(os.path.realpath(__file__))
+		options.fragmentDir = os.path.join(workingdir,'config_fragments')
+
+	configurator = daq2EvBIEConfigurator(options.fragmentDir,
+		                                verbose=options.verbose)
+
+	configurator.evbns = 'evb'
+
+	## Pass options
+	configurator.RUSendPoolSize = options.RUSendPoolSize
+	configurator.RUSendQPSize   = options.RUSendQPSize
+	configurator.RUComplQPSize  = options.RUComplQPSize
+	configurator.BURecvPoolSize = options.BURecvPoolSize
+	configurator.BURecvQPSize   = options.BURecvQPSize
+	configurator.BUComplQPSize  = options.BUComplQPSize
+
+	configurator.setDynamicIBVConfig = options.setDynamicIBVConfig
+
+	## Construct output name
+	output = args[0]
+	output += '_evb'
+	output+='_ibv'
+	output+='.xml'
+
+	if len(options.output)>0:
+		name, ext = os.path.splitext(options.output)
+		if not os.path.dirname(name) == '':
+			try:
+				os.makedirs(os.path.dirname(name))
+			except OSError as e:
+				if not 'File exists' in str(e):
+					raise e
+
+		if ext == '.xml':
+			# Take exactly what's given in the option
+			output = options.output
+		elif ext == '':
+			output = os.path.join(name, output)
+
+	configurator.makeEvBIEConfig(nRUs, nBUs, output)
+
+	return True
+
+if __name__ == "__main__":
+	from optparse import OptionParser
+	usage = """
+	%prog [options] topology
+	where topology is in the format of nRUs x nBUs,
+	e.g. 4x2
+
+	Examples:
+	%prog --useGevb2g 4x4
+	%prog 2x4 --fragmentDir fragments/ -o 2x4_special.xml
+	"""
+	parser = OptionParser()
+	parser.usage = usage
+	addConfiguratorOption(parser)
+	(options, args) = parser.parse_args()
+
+	if len(args) > 0:
+		if main(options, args):
+			exit(0)
+		else:
+			printError("Something went wrong.")
+
+	parser.print_help()
+	exit(-1)
+
