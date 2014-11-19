89c7324f919490a2d2fa9cf0ca7ae9c90275ac03
Benjamin Stieger <stiegerb@cern.ch>
2014-11-19 21:06:40 +0100

    Added 40GE cabling to printSwitchCabling.

--- daq2Control/printSwitchCabling.py
+++ daq2Control/printSwitchCabling.py
@@ -7,32 +7,45 @@ if __name__ == "__main__":
 	from optparse import OptionParser
 	usage = """"""
 	parser = OptionParser(usage=usage)
-	parser.add_option("-i", "--inventoryFile",
-		               default="2014-10-15-infiniband-ports.csv",
-		               action="store", type="string", dest="inventoryFile",
-		               help=("Inventory file [default: %default]"))
 	(opt, args) = parser.parse_args()
 
-	hwInfo = daq2HardwareInfo(ibcabling=opt.inventoryFile,verbose=0)
-
-	# pprint(switch_cabling)
+	hwInfo = daq2HardwareInfo()
 
 	list_of_hosts = []
 
 	sm = daq2SymbolMap(args[0])
+	print 80*'-'
 	for key in sorted(sm.keys()):
 		if not 'SOAP_HOST_NAME' in key: continue
 		if 'FEROLCONTROLLER' in key: continue ## exclude 40GE now
 		tag = key.split('_',1)[0]
 		switchname,port = hwInfo.ib_host_cabling[sm(key).strip('.cms')]
-		# print "%5s, %18s, %17s, %2d" % (tag,  sm(key), switchname, port)
 		list_of_hosts.append((port, tag, sm(key).strip('.cms')))
 
 	for switch in hwInfo.ib_switch_cabling.keys():
-		switch_hosts = [host for host,_ in hwInfo.ib_switch_cabling[switch].values()]
+		switch_hosts = [host for host,_ in
+		                        hwInfo.ib_switch_cabling[switch].values()]
 		print switch
 		for port,tag,host in list_of_hosts:
 			if host in switch_hosts:
-				print '    %2d: %5s %14s' % (port, tag, host)
+				print '    %2d: %-17s %14s' % (port, tag, host)
+
+	list_of_hosts = []
+	print 80*'-'
+	for key in sorted(sm.keys()):
+		if not 'SOAP_HOST_NAME' in key: continue
+		if 'BU' in key: continue ## exclude IB
+		tag = key.split('_',1)[0]
+		switchname = hwInfo.ge_host_cabling[sm(key).strip('.cms')]
+		list_of_hosts.append((tag, sm(key).strip('.cms')))
+
+	for switch in hwInfo.ge_switch_cabling.keys():
+		switch_hosts = [host for host in
+		                        hwInfo.ge_switch_cabling[switch]]
+		print switch
+		for tag,host in list_of_hosts:
+			if host in switch_hosts:
+				print '      : %-17s %14s' % (tag, host)
+	print 80*'-'
 
 	exit(0)
